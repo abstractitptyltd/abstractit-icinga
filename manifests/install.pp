cdabef711646052380fbe70a01753cc223b5ee28
Pete Brown <pete@abstractit.com.au>
2014-12-15 11:48:23 +1000

    new apache includes apache mod authn_file releasing 1.2.2-rc4

--- manifests/install.pp
+++ manifests/install.pp
@@ -3,11 +3,10 @@
 # install necessicary packages for icinga
 
 class icinga::install {
-
   include icinga::params
 
   case $::osfamily {
-    'RedHat': {
+    'RedHat' : {
       if ($::operatingsystem != 'Fedora') {
         yumrepo { "icinga-stable-release-${::operatingsystemmajrelease}":
           enabled  => 1,
@@ -16,15 +15,19 @@ class icinga::install {
           gpgkey   => 'http://packages.icinga.org/icinga.key',
         }
       }
-      package { ['icinga',"icinga-idoutils-libdbi-${icinga::params::ido_db_server}"]:
+
+      package { [
+        'icinga',
+        "icinga-idoutils-libdbi-${icinga::params::ido_db_server}"]:
         ensure  => present,
         require => $::operatingsystem ? {
           'Fedora' => undef,
           default  => Yumrepo["icinga-stable-release-${::operatingsystemmajrelease}"],
         }
       }
-#      ensure_packages(['icinga',"icinga-idoutils-libdbi-${icinga::params::ido_db_server}"])
-      if ( $icinga::params::gui_type =~ /^(classic|both)$/ ) {
+
+      #      ensure_packages(['icinga',"icinga-idoutils-libdbi-${icinga::params::ido_db_server}"])
+      if ($icinga::params::gui_type =~ /^(classic|both)$/) {
         package { 'icinga-gui':
           ensure  => present,
           require => $::operatingsystem ? {
@@ -32,36 +35,49 @@ class icinga::install {
             default  => Yumrepo["icinga-stable-release-${::operatingsystemmajrelease}"],
           }
         }
-#        ensure_packages(['icinga-gui'])
+        #        ensure_packages(['icinga-gui'])
       }
-      if ( $icinga::params::gui_type =~ /^(web|both)$/ ) {
-        package { ['icinga-web','php-soap','php-gd','php-ldap']:
+
+      if ($icinga::params::gui_type =~ /^(web|both)$/) {
+        package { [
+          'icinga-web',
+          'php-soap',
+          'php-gd',
+          'php-ldap']:
           ensure => present,
         }
-#        ensure_packages(['icinga-web','php-soap','php-gd','php-ldap'])
+        #        ensure_packages(['icinga-web','php-soap','php-gd','php-ldap'])
       }
     }
-    'Debian': {
+    'Debian' : {
       include apt
-      apt::ppa { 'ppa:formorer/icinga': }
+
+      apt::ppa { 'ppa:formorer/icinga':
+      }
+
       if $icinga::params::gui_type =~ /^(web|both)$/ {
-        apt::ppa { 'ppa:formorer/icinga-web': }
+        apt::ppa { 'ppa:formorer/icinga-web':
+        }
+
         package { 'icinga-web':
           ensure  => present,
           require => Apt::Ppa['ppa:formorer/icinga-web'],
         }
-#        ensure_packages(['icinga-web'])
-        if ! defined(Apache::Mod['authn_file']) {
-          apache::mod{'authn_file':}
-        }
+        #        not needed anymore
+        #        if ! defined(Apache::Mod['authn_file']) {
+        #          apache::mod{'authn_file':}
+        #        }
       }
-      package { ['icinga','icinga-idoutils']:
+
+      package { [
+        'icinga',
+        'icinga-idoutils']:
         ensure  => present,
         require => Apt::Ppa['ppa:formorer/icinga'],
       }
-#      ensure_packages(['icinga','icinga-idoutils'])
-      }
-    default: {
+      #      ensure_packages(['icinga','icinga-idoutils'])
+    }
+    default  : {
       fail("Only support Red Hat and Debian type systems, not ${::osfamily}")
     }
   }
